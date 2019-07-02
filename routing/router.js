fac69dd8a3f1333d6954a73bb2bc9bde33ec0954
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-07-02 12:42:33 -0500

    adding survey

--- routing/router.js
+++ routing/router.js
@@ -1,12 +1,16 @@
 var path = require("path");
 
-module.export = function(app) {
+module.exports = function(app) {
   // Survey HTML Path
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../app/public/survey.html"));
   });
   //   View HTML Path
   app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../app/public/view.html"));
+  });
+
+  app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../app/public/view.html"));
   });
 };
